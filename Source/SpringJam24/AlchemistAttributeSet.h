976a639bf816fad6c645052fa3d1b1b2d220b1ae
Ryan <ryan.sutherland217@gmail.com>
2024-05-03 20:47:20 -0500

    I don't know where these external files came from, but they gots to go.

---
+++ Source/SpringJam24/AlchemistAttributeSet.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AlchemistAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPRINGJAM24_API UAlchemistAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+	
+};
